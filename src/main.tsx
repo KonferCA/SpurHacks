3ec9ef93c17c13a12a377cc505651a1a6408a523
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-04-07 20:53:21 -0400

    point to router + format + lint

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
+import { Router } from '@utils';
 
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+createRoot(rootElement).render(
+    <StrictMode>
+        <Router />
     </StrictMode>
 );
