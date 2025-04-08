d6c70183a1b2c6a3aa662139862622fa3b30549b
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-04-07 20:30:10 -0400

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
+import tailwindcss from '@tailwindcss/vite'
 
 /*
  *
@@ -10,7 +11,9 @@ import * as path from 'path';
 export default defineConfig({
     plugins: [
         react(),
+        tailwindcss(),
     ],
+    
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
