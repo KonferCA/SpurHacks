d0d4d36d3b633521a32eb4a432d5022a68592bd8
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-04-11 21:32:23 -0400

    [working] sponsorship section

--- src/components/sections/sponsorship.section.tsx
+++ src/components/sections/sponsorship.section.tsx
@@ -1,23 +1,69 @@
+import { Alert, Heading, Text, Button, Flex } from "@chakra-ui/react";
+
 export const Sponsorship = () => {
-    return (
-        <div className="bg-black text-white min-h-screen grid place-items-center">
-            <div className="flex items-center justify-center w-full h-full text-center bg-[linear-gradient(to_right,rgba(255,255,255,0.05)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.05)_1px,transparent_1px)] [background-size:60px_60px]">
-                {/* Header and Text */}
-                <div className="flex flex-col gap-2">
-                    <h1 className="!text-4xl">Sponsor a Special Weekend</h1>
-                    <p className="">
-                        Placerat maecenas aliquam primis duis viverra integer.
-                        Vehicula nulla bibendum facilisis per quis vehicula
-                        risus donec euismod. Curabitur aliquet sem vel fermentum
-                        lacinia. Aliquam sodales neque lorem, aliquam luctus
-                        tellus viverra ut. Curabitur.
-                    </p>
-                    <p>
-                        Vehicula nulla bibendum facilisis per
-                        <strong> sponsors@spurhacks.com.</strong>
-                    </p>
-                </div>
-            </div>
-        </div>
-    );
+	const quantaSponsors = [
+		{
+			name: "Company",
+			image: "image here",
+		},
+	];
+
+	const quarkSponsors = [
+		{
+			name: "Company",
+			image: "image here",
+		},
+	];
+
+	const entanglementSponsors = [
+		{
+			name: "Company",
+			image: "image here",
+		},
+	];
+
+	const singularitySponsors = [
+		{
+			name: "Company",
+			image: "image here",
+		},
+	];
+
+	return (
+		<div className="bg-black text-white min-h-screen grid place-items-center">
+			<Flex className="flex flex-col gap-20 items-center justify-center w-full h-full text-center bg-[linear-gradient(to_right,rgba(255,255,255,0.15)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.15)_1px,transparent_1px)] [background-size:60px_60px]">
+				{/* Header and Text */}
+				<div className="flex flex-col items-center justify-center gap-8 max-w-3xl">
+					<Heading fontSize={{ base: "sm", desktop: "5xl" }}>
+						Sponsor a Special Weekend
+					</Heading>
+					<Text>
+						Placerat maecenas aliquam primis duis viverra integer. Vehicula
+						nulla bibendum facilisis per quis vehicula risus donec euismod.
+						Curabitur aliquet sem vel fermentum lacinia. Aliquam sodales neque
+						lorem, aliquam luctus tellus viverra ut. Curabitur.
+					</Text>
+					<Text>
+						Vehicula nulla bibendum facilisis per
+						<strong> sponsors@spurhacks.com.</strong>
+					</Text>
+					<Button
+						color="brand.orange"
+						// className="bg-[#FFA75F] font-semibold uppercase py-3 px-5 rounded-full text-black"
+					>
+						Become a sponsor
+					</Button>
+				</div>
+				<div>
+					<Alert.Root>
+						<Alert.Indicator />
+						<Alert.Content>
+							<Alert.Title>test</Alert.Title>
+							<Alert.Description>test desc</Alert.Description>
+						</Alert.Content>
+					</Alert.Root>
+				</div>
+			</Flex>
+		</div>
+	);
 };
