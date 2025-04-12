ff9a6dda30e4aa7e819e8dc7d139380bfff4334d
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-04-11 21:31:18 -0400

    [add] sponsorship to landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,3 +1,13 @@
+import { Sponsorship } from "@components";
+
 export const Landing = () => {
-    return <div>Landing</div>;
+	return (
+		<div>
+			<main>
+				<div id="sponsorship">
+					<Sponsorship />
+				</div>
+			</main>
+		</div>
+	);
 };
