7a96c44bae31c08f48f5ff927f4dbc97ea9d25f0
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-04-12 22:36:21 -0400

    [import] temporary images for sponsorship

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,6 +3,33 @@
  * @asset
  *
  */
-import LogoWhite from './spurhacks-logo-white.png';
+import LogoWhite from "./spurhacks-logo-white.png";
 
 export { LogoWhite };
+
+/*
+ *
+ * @asset Quanta Sponsors
+ *
+ */
+import Quanta from "./sponsors/quantaSponsors/quant.png";
+/*
+ *
+ * @asset Quark Sponsors
+ *
+ */
+import Quark from "./sponsors/quarkSponsors/quark.png";
+/*
+ *
+ * @asset Entanglement Sponsors
+ *
+ */
+import Entang from "./sponsors/entanglementSponsors/entang.png";
+/*
+ *
+ * @asset Singularity Sponsors
+ *
+ */
+import Sing from "./sponsors/singularitySponsors/sing.png";
+
+export { Quanta, Quark, Entang, Sing };
