9a63f0c536c93c255ecb91499483d44ee648dcf9
Nausher Rao <sherrao15@gmail.com>
2025-04-14 18:10:38 -0400

    Add initial code for spurhacks coming soon site

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import { Provider } from '@components';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "@components";
+import { Router } from "@utils";
 
-import { Router } from '@utils';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Failed to find the root element');
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
 
 createRoot(rootElement).render(
     <StrictMode>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,3 +1,35 @@
+import React from "react";
+import Spline from "@splinetool/react-spline";
+import { LogoWhite } from "../assets/";
+
 export const Landing = () => {
-    return <div>Landing</div>;
-};
+  return (
+    <main>
+        <div className="w-screen h-screen flex flex-col items-center justify-center">
+            <Spline
+                scene="https://prod.spline.design/TmAYMNy2qJHyDE9m/scene.splinecode"
+                className="w-screen h-screen absolute top-0 left-0 z-0"
+            />
+            <div className="font-[Geist] text-9xl text-white text-bold w-full h-full z-10 flex flex-col items-center justify-center gap-10">
+                <img src={LogoWhite} className="" />
+
+                <div className="flex flex-row items-center justify-center gap-10">
+                    <p>June 20-22, 2025 </p>
+                    <span>|</span>
+                    <p>In-person</p>
+                    <span>|</span>
+                    <p>Waterloo, ON</p>
+                </div>
+
+                <p>
+                    <a href="https://linktr.ee/spurhacks"
+                        target="_blank">
+                        Coming soon - stay tuned!
+                    </a>
+                </p>
+            </div>
+        </div>
+    </main>
+  );
+}
+
