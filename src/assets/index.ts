0431eb779991a41c38fa0087e818e74549b6f632
Nausher Rao <sherrao15@gmail.com>
2025-04-16 16:14:53 -0400

    Update assets index file

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,6 +4,7 @@
  *
  */
 import VerticalLogo from './logo_vertical.svg';
-import HorizontalLogo from './logo_horizontalsvg';
+import HorizontalLogo from './logo_horizontal.svg';
+import Icon from './icon.svg';
 
-export { VerticalLogo, HorizontalLogo };
+export { VerticalLogo, HorizontalLogo, Icon };
