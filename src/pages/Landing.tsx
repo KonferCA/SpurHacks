3d6dee5960fb0e0a6813f710da3d2b75655da5a1
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-04-17 19:49:03 -0400

    remove navbar

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,7 +3,7 @@ import { Navbar, Hero } from '@components';
 export const Landing = () => {
     return (
         <>
-            <Navbar />
+            {/* <Navbar /> */}
             <Hero />
         </>
     );
