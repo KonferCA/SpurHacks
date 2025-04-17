129c17202c0659616f0d464d3b7b24616ae28146
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-04-16 20:22:53 -0400

    [update] chakra provider config

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,52 +1,49 @@
 "use client";
 import {
-	ChakraProvider,
-	defineConfig,
-	defaultConfig,
-	createSystem,
+    ChakraProvider,
+    defineConfig,
+    defaultConfig,
+    createSystem,
 } from "@chakra-ui/react";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
-// import { PrimitiveTokenValue }
+
 const spurhacksDesignConfig = defineConfig({
-	strictTokens: false,
-	theme: {
-		breakpoints: {
-			tablet: "992px",
-			desktop: "1200px",
-			wide: "1400px",
-		},
-		tokens: {
-			colors: {
-				brand: {
-					orange: { value: "#FFA75F" },
-					100: { value: "#e6f2ff" },
-					200: { value: "#bfdeff" },
-					300: { value: "#99caff" },
-				},
-			},
-		},
-		semanticTokens: {
-			colors: {
-				danger: { value: "{colors.red}" },
-			},
-		},
-		keyframes: {
-			spin: {
-				from: { transform: "rotate(0deg)" },
-				to: { transform: "rotate(360deg)" },
-			},
-		},
-	},
+    strictTokens: false,
+    theme: {
+        breakpoints: {
+            tablet: "992px",
+            desktop: "1200px",
+            wide: "1400px",
+        },
+        tokens: {
+            colors: {
+                orange: {
+                    default: { value: "#FFA75F" },
+                    hover: { value: "#e6f2ff" },
+                },
+                offWhite: { value: "#DEEBFF" },
+            },
+        },
+        semanticTokens: {
+            colors: {
+                danger: { value: "{colors.red}" },
+            },
+        },
+        keyframes: {
+            spin: {
+                from: { transform: "rotate(0deg)" },
+                to: { transform: "rotate(360deg)" },
+            },
+        },
+    },
 });
 
 const spurhacksSystem = createSystem(defaultConfig, spurhacksDesignConfig);
 
 export function Provider(props: ColorModeProviderProps) {
-	return (
-		// <CacheProvider value={emotionCache}>
-		<ChakraProvider value={spurhacksSystem}>
-			<ColorModeProvider {...props} />
-		</ChakraProvider>
-		// </CacheProvider>
-	);
+    return (
+        <ChakraProvider value={spurhacksSystem}>
+            <ColorModeProvider {...props} />
+        </ChakraProvider>
+    );
 }
