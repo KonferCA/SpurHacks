3029204c5262e268170f90dcead1668122d66298
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-04-18 22:26:39 -0400

    [update] quanta sponsors and add partners

--- src/components/sections/sponsorship.section.tsx
+++ src/components/sections/sponsorship.section.tsx
@@ -8,6 +8,7 @@ import {
 	Image,
 	Grid,
 	Stack,
+	Link,
 } from "@chakra-ui/react";
 
 export const Sponsorship = () => {
@@ -71,6 +72,33 @@ export const Sponsorship = () => {
 			name: "This Company",
 			image: Quanta,
 		},
+		{
+			name: "This Company",
+			image: Quanta,
+		},
+		{
+			name: "This Company",
+			image: Quanta,
+		},
+		{
+			name: "This Company",
+			image: Quanta,
+		},
+	];
+
+	const partners = [
+		{
+			name: "Konfer",
+			image: Quanta,
+		},
+		{
+			name: "Konfer",
+			image: Quanta,
+		},
+		{
+			name: "Konfer",
+			image: Quanta,
+		},
 	];
 
 	// Reusable width for all tiers
@@ -110,16 +138,18 @@ export const Sponsorship = () => {
 						Vehicula nulla bibendum facilisis per
 						<strong> sponsors@spurhacks.com.</strong>
 					</Text>
-					<Button
-						color="black"
-						background="orange.default"
-						rounded="full"
-						p={5}
-						_hover={{ bg: "orange.hover" }}
-						transition="all 0.3s ease-in-out"
-					>
-						BECOME A SPONSOR
-					</Button>
+					<Link href="https://konfer.ca">
+						<Button
+							color="black"
+							background="orange.default"
+							rounded="full"
+							p={5}
+							_hover={{ bg: "orange.hover" }}
+							transition="all 0.3s ease-in-out"
+						>
+							BECOME A SPONSOR
+						</Button>
+					</Link>
 				</Flex>
 
 				<Stack gap={{ base: 10, lg: 20 }} align="center" w="90%">
@@ -144,7 +174,7 @@ export const Sponsorship = () => {
 									<Image
 										src={sponsor.image}
 										alt={sponsor.name}
-										boxSize={{ base: "150px", md: "200px" }}
+										boxSize={{ base: "200px", md: "180px" }}
 										objectFit="contain"
 										py={6}
 									/>
@@ -279,18 +309,57 @@ export const Sponsorship = () => {
 					</Box>
 					{/* QUANTA SPONSORS (4TH) */}
 					<Box w="full" maxW={MAX_WIDTH} mx="auto">
+						<Flex
+							wrap="wrap"
+							justify="center"
+							gap={6}
+							maxW={MAX_WIDTH}
+							mx="auto"
+							w="full"
+						>
+							{quantaSponsors.map((sponsor, index) => (
+								<Box
+									key={index}
+									bg="offWhite"
+									rounded="3xl"
+									flex={{ base: "1 1 45%", md: "1 1 21%" }}
+									maxW={{ base: "45%", md: "22%" }}
+									minW="150px"
+									h={{ base: "100px", md: "125px" }}
+									display="flex"
+									alignItems="center"
+									justifyContent="center"
+								>
+									<Image
+										src={sponsor.image}
+										alt={sponsor.name}
+										boxSize={{ base: "100px", md: "125px" }}
+										objectFit="contain"
+										py={5}
+									/>
+								</Box>
+							))}
+						</Flex>
+					</Box>
+				</Stack>
+				<Stack gap={{ base: 10, lg: 20 }} align="center" w="90%">
+					<Heading fontSize={["2xl", "3xl", "4xl", "5xl"]}>
+						Our Partners
+					</Heading>
+					{/* QUARK SPONSORS (3RD) */}
+					<Box w="full" maxW={MAX_WIDTH} mx="auto">
 						<Grid
 							templateColumns={{
-								base: "repeat(3, 1fr)",
-								md: "repeat(4, 1fr)",
+								base: "repeat(2, 1fr)",
+								md: "repeat(3, 1fr)",
 							}}
 							gap={{ base: 6, lg: 10 }}
 							justifyItems="center"
 						>
-							{quantaSponsors.map((sponsor, index) => {
+							{partners.map((sponsor, index) => {
 								const isLastItem =
-									index === quantaSponsors.length - 1;
-								const isOdd = quantaSponsors.length % 2 === 1;
+									index === quarkSponsors.length - 1;
+								const isOdd = quarkSponsors.length % 2 === 1;
 
 								return (
 									<Box
@@ -329,7 +398,7 @@ export const Sponsorship = () => {
 											alt={sponsor.name}
 											boxSize={{
 												base: "100px",
-												md: "150px",
+												md: "125px",
 											}}
 											objectFit="contain"
 											py={5}
@@ -340,11 +409,6 @@ export const Sponsorship = () => {
 						</Grid>
 					</Box>
 				</Stack>
-				<Flex direction="column">
-					<Heading fontSize={["2xl", "3xl", "4xl", "5xl"]}>
-						Our Partners
-					</Heading>
-				</Flex>
 			</Flex>
 		</div>
 	);
