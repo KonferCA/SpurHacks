d17e11b679704494627ed67f6ace2b3a4c6d7f85
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-04-20 00:42:39 -0400

    [rebase] stats-section

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,12 @@
-import { Sponsorship } from '@components';
+import { Statistics, Sponsorship } from "@components";
 
 export const Landing = () => {
     return (
         <div>
             <main>
+                <div id="statistics">
+                    <Statistics />
+                </div>
                 <div id="sponsorship">
                     <Sponsorship />
                 </div>
