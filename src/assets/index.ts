300b8318c835e6dbc4a41f964451f6dafbf3c0df
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-04-21 14:19:09 -0400

    [update] index and remove sponsorship pdf from assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,60 +3,9 @@
  * @asset
  *
  */
-import VerticalLogo from './logo_vertical.svg';
-import HorizontalLogo from './logo_horizontal.svg';
-import Icon from './icon.svg';
+import VerticalLogo from "./logo_vertical.svg";
+import HorizontalLogo from "./logo_horizontal.svg";
+import Icon from "./icon.svg";
+import SponsorData from "./data/sponsors.json";
 
-export { VerticalLogo, HorizontalLogo, Icon };
-/*
- *
- * @asset Sponsorship package
- *
- */
-import SponsorshipPackage from './spurhacks-sponsorship-package.pdf';
-
-export { SponsorshipPackage };
-
-/*
- *
- * @asset Quanta Sponsors
- *
- */
-import Quanta from './sponsors/quantaSponsors/quant.png';
-
-export const quantaImages = {
-    quantaEx: Quanta,
-};
-
-/*
- *
- * @asset Quark Sponsors
- *
- */
-import Quark from './sponsors/quarkSponsors/quark.png';
-
-export const quarkImages = {
-    quarkEx: Quark,
-};
-
-/*
- *
- * @asset Entanglement Sponsors
- *
- */
-import Entang from './sponsors/entanglementSponsors/entang.png';
-
-export const entanglementImages = {
-    entanglementEx: Entang,
-};
-
-/*
- *
- * @asset Singularity Sponsors
- *
- */
-import Sing from './sponsors/singularitySponsors/sing.png';
-
-export const singularityImages = {
-    singularityEx: Sing,
-};
+export { VerticalLogo, HorizontalLogo, Icon, SponsorData };
