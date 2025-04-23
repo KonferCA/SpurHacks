e70e67ac9143f90c5b609a7be7c0c2fe5e2aefab
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-04-23 14:59:58 -0400

    add logo icons + exports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -9,3 +9,11 @@ import Icon from './icon.svg';
 import SponsorData from './data/sponsors.json';
 
 export { VerticalLogo, HorizontalLogo, Icon, SponsorData };
+
+import LogoNoIcon from './logo_noicon.svg';
+import SpurNoIcon from './spur_noicon.svg';
+import KonferNoIcon from './konfer_noicon.svg';
+
+export { LogoNoIcon };
+export { SpurNoIcon };
+export { KonferNoIcon };
