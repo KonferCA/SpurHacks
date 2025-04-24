4a2fdbc13f0cb58292fa15d73af14a7cc1e869be
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-04-23 20:24:07 -0400

    [fix] rebase issue

--- src/components/index.ts
+++ src/components/index.ts
@@ -39,14 +39,9 @@ export { Statistics };
  * @section Sponsorship
  *
  */
-import { Statistics } from './sections/statistics.section';
 import { Sponsorship } from './sections/sponsorship.section';
 
-<<<<<<< HEAD
-export { Sponsorship };
-=======
-export { Statistics, Sponsorship };
->>>>>>> afcb413d0dfc66cd9cf000a6830d72384e5ee557
+export { Sponsorship }
 
 /*
  *
@@ -56,10 +51,7 @@ export { Statistics, Sponsorship };
 import { CountUp } from './countup';
 
 export { CountUp };
-<<<<<<< HEAD
 
 import { Footer } from './Footer';
 
 export { Footer };
-=======
->>>>>>> afcb413d0dfc66cd9cf000a6830d72384e5ee557
