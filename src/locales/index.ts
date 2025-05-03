d7eee6bca1422f5b8c05735171802a44864a049e
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-05-03 05:23:30 -0400

    add locale exports

---
+++ src/locales/index.ts
@@ -0,0 +1,69 @@
+import strings from './en.json';
+
+/*
+ *
+ * @object Strings
+ *
+ */
+export { strings };
+
+/*
+ *
+ * @object / section Navbar
+ *
+ */
+export const navStrings = strings.navigation;
+export const navLinks = strings.navigation.links;
+export const navButtons = strings.navigation.buttons;
+export const mlhStrings = strings.navigation.mlh;
+
+/*
+ *
+ * @object / section FAQ
+ *
+ */
+export const faqStrings = strings.faq;
+export const faqItems = strings.faq.items;
+
+/*
+ *
+ * @object / section Hero
+ *
+ */
+export const heroStrings = strings.hero;
+export const heroEventDetails = strings.hero.eventDetails;
+export const heroButtons = strings.hero.buttons;
+export const heroCountdown = strings.hero.countdown;
+export const heroSocialMedia = strings.hero.socialMedia;
+
+/*
+ *
+ * @object / section Sponsors
+ *
+ */
+export const sponsorshipStrings = strings.sponsorship;
+
+/*
+ *
+ * @object / section Statistics
+ *
+ */
+export const statisticsItems = strings.statistics;
+
+/*
+ *
+ * @object / section Updates
+ *
+ */
+export const updatesStrings = strings.updates;
+
+/*
+ *
+ * @object / section Footer
+ *
+ */
+export const footerStrings = strings.footer;
+export const footerSocialMedia = strings.footer.socialMedia;
+export const footerLinks = strings.footer.links;
+
+export default strings;
