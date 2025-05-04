00cf0c6a0a482e14bdea23ed46e2739bfdffa889
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-05-04 00:01:12 -0400

    add data exports

---
+++ src/data/index.ts
@@ -0,0 +1,28 @@
+import sponsorsData from './sponsors.json';
+
+/*
+ *
+ * @object Sponsors Data
+ *
+ */
+export { sponsorsData };
+
+/*
+ *
+ * @object / section Sponsor Categories
+ *
+ */
+export const singularitySponsors = sponsorsData.singularitySponsors;
+export const entanglementSponsors = sponsorsData.entanglementSponsors;
+export const quarkSponsors = sponsorsData.quarkSponsors;
+export const quantaSponsors = sponsorsData.quantaSponsors;
+export const partners = sponsorsData.partners;
+
+/*
+ *
+ * @object / section Resource Paths
+ *
+ */
+export const sponsorsBaseUrl = '/src/assets/sponsors/';
+
+export default sponsorsData;
