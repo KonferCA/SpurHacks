b45458a15844843755a18730ce0c50faa2d1dfc1
Nausher Rao <sherrao15@gmail.com>
2025-05-05 15:56:30 -0400

    lint

--- src/assets/sponsors/partners/index.ts
+++ src/assets/sponsors/partners/index.ts
@@ -1,7 +1,7 @@
-import KonferLogo from "./konfer.svg";
-import SpurLogo from "./spur.svg";
+import KonferLogo from './konfer.svg';
+import SpurLogo from './spur.svg';
 import LCSLogo from './lcs.svg';
-import HawkHacksLogo from "./hawkhacks.svg";
-import WaterlooBlockchainLogo from "./uwb.svg";
+import HawkHacksLogo from './hawkhacks.svg';
+import WaterlooBlockchainLogo from './uwb.svg';
 
 export { KonferLogo, SpurLogo, LCSLogo, HawkHacksLogo, WaterlooBlockchainLogo };
