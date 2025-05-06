7b9f21e9f7efc00609d438a2c2c37c127b6efc94
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-05-06 06:39:08 -0400

    update logic using vite meta

--- src/utils/assetLoader.ts
+++ src/utils/assetLoader.ts
@@ -1,15 +1,11 @@
 export function getAssetUrl(path: string) {
-    try {
-        if (path.endsWith('.svg')) {
-            return `/src/assets/sponsors/${path}`;
-        }
-
-        const assetModule = `/src/assets/sponsors/${path}`;
-
-        return assetModule;
-    } catch (error) {
-        console.error(`Failed to load asset: ${path}`, error);
-
-        return `/failed-to-load/${path}`;
+    const isProd = import.meta.env.PROD;
+    
+    // production
+    if (isProd) {
+        return `/sponsors/${path}`;
     }
+
+    // development
+    return `/src/assets/sponsors/${path}`;
 }
